de6fc9a69469e5c70ffe3e4ecafe024330a785e4
Anton Povarov <anton.povarov@gmail.com>
2025-12-09 17:28:40 +0400

    Go: added testutil to run tests conveniently

---
+++ go/leetcode/testutil/testutil.go
@@ -0,0 +1,55 @@
+package testutil
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/fatih/color"
+)
+
+func isOK[T any](result, expected T) bool {
+	return reflect.DeepEqual(result, expected)
+}
+
+func isOKStr[T any](result, expected T) string {
+	if reflect.DeepEqual(result, expected) {
+		return "OK"
+	}
+	return "ERROR"
+}
+
+type TestData[T any, E any] struct {
+	Input    T
+	Expected E
+}
+
+func RunTest[T any, E comparable](t *testing.T, testedFunc func(T) E, data []TestData[T, E]) {
+	green := color.New(color.FgGreen)
+	red := color.New(color.FgHiRed)
+
+	results := make([]E, len(data))
+
+	for i, d := range data {
+		result := testedFunc(d.Input)
+		results[i] = result
+
+		clr := func() *color.Color {
+			if reflect.DeepEqual(result, d.Expected) {
+				return green
+			}
+			return red
+		}()
+
+		clr.EnableColor() // this is required as go test does shenanigans with colored output
+		fmt.Fprintln(t.Output(),
+			clr.Sprintf("[%s] %#v -> %#v, expected: %#v", isOKStr(result, d.Expected), d.Input, result, d.Expected))
+		clr.DisableColor()
+	}
+
+	for i, d := range data {
+		if results[i] != d.Expected {
+			t.FailNow()
+		}
+	}
+}
